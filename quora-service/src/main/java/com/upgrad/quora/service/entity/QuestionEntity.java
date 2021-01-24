e8d5a9fbd416d5cb5554ec5a64f10923e12954a6
Adarsha Acharya <adarshrya@users.noreply.github.com>
2021-01-24 21:04:40 +0530

    Imports for #27

--- quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
@@ -15,6 +15,8 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name = "question")
